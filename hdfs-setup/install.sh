7b5051041da3100a84eeea2689624b951a396d23
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-13 16:51:32 -0300

    script

--- hdfs-setup/install.sh
+++ hdfs-setup/install.sh
@@ -7,5 +7,13 @@ sudo apt install openjdk-8-jdk
 echo "JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64/bin/\" > ~/.bashrc
 source ~/.bashrc
 
+java -version
+
 wget https://downloads.apache.org/hadoop/common/hadoop-3.3.0/hadoop-3.3.0-aarch64.tar.gz
 tar -xvf hadoop-3.3.0-aarch64.tar.gz
+cd hadoop-3.3.0-aarch64/
+mkdir input/
+cp etc/hadoop/*.xml input
+bin/hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-3.2.1.jar grep input output 'dfs[a-z.]+'
+cat output/*
+
