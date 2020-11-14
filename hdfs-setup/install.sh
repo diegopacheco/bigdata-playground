26f1ec38570a52939e5bcb5f08dd1b2606d79222
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-14 13:25:04 -0300

    improve script

--- hdfs-setup/install.sh
+++ hdfs-setup/install.sh
@@ -13,7 +13,7 @@ java -version
 wget -nc https://downloads.apache.org/hadoop/common/hadoop-3.3.0/hadoop-3.3.0-aarch64.tar.gz
 tar -xvf hadoop-3.3.0-aarch64.tar.gz
 cd hadoop-3.3.0/
-mkdir input/
+mkdir -p input/
 cp etc/hadoop/*.xml input
 bin/hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-3.2.1.jar grep input output 'dfs[a-z.]+'
 cat output/*
