e857af4e817229b66539b25bc389f56b71c42a1b
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-16 12:16:59 -0300

    improve script

--- hdfs-setup/install-spark.sh
+++ hdfs-setup/install-spark.sh
@@ -4,9 +4,10 @@
 ## Scala 
 ##
 
+cd /home/vagrant/
+
 wget -nc https://downloads.lightbend.com/scala/2.13.3/scala-2.13.3.tgz
 tar -xvf scala-2.13.3.tgz
-mv scala-2.13.3/ /home/vagrant/
 
 export SCALA_HOME=/home/vagrant/scala-2.13.3/
 export PATH=$PATH:$SCALA_HOME/bin
@@ -24,6 +25,8 @@ scala -version
 
 wget -nc https://downloads.apache.org/spark/spark-3.0.1/spark-3.0.1-bin-hadoop3.2.tgz
 tar -xvf spark-3.0.1-bin-hadoop3.2.tgz
-mv spark-3.0.1-bin-hadoop3.2/ /home/vagrant/ 
 
 export SPARK_HOME=/home/vagrant/spark-3.0.1-bin-hadoop3.2/
+sh -c 'echo "export SPARK_HOME=\"/home/vagrant/spark-3.0.1-bin-hadoop3.2/\"" >> ~/.bashrc'
+sudo sh -c 'echo "export SPARK_HOME=\"/home/vagrant/spark-3.0.1-bin-hadoop3.2/\"" >> /etc/profile'
+
