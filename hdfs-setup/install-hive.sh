5436de30fb46e25edcb294f635e94c7f38613148
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-16 13:02:10 -0300

    improve script

--- hdfs-setup/install-hive.sh
+++ hdfs-setup/install-hive.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+cd /home/vagrant/
+
 wget -nc https://downloads.apache.org/hive/hive-3.1.2/apache-hive-3.1.2-bin.tar.gz
 tar -xvf apache-hive-3.1.2-bin.tar.gz
-mv apache-hive-3.1.2-bin/ /home/vagrant/
+ 
 export HIVE_HOME=/home/vagrant/apache-hive-3.1.2-bin/
+
+sh -c 'echo "export HIVE_HOME=\"/home/vagrant/apache-hive-3.1.2-bin/\"" >> ~/.bashrc'
+sudo sh -c 'echo "export HIVE_HOME=\"/home/vagrant/apache-hive-3.1.2-bin/\"" >> /etc/profile'
+
